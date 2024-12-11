38e07dbbdee948b185f5d028155195c425cfb41b
sultangareevtk <sultangareevtagir@gmail.com>
2024-12-11 14:12:38 +0500

    Create test2.php

---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Test 2
+Plugin URI: https://nextendweb.com/
+Description: Nextend Social Login displays social login buttons for Facebook, Google and X (formerly Twitter).
+Version: 3.1.16
+Requires PHP: 7.0
+Requires at least: 4.9
+Author: Nextendweb
+Author URI: https://nextendweb.com/social-login/
+License: GPL2
+Text Domain: nextend-facebook-connect
+Domain Path: /languages
+*/
